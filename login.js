d9dc67e2380dfdc009f2730df20eb22f1994bd22
clebs0n <95928023+clebs0n@users.noreply.github.com>
2023-02-19 13:14:38 -0300

    Second commit
    This code can compares the name with the records in the database and succesfully auths if the records matchs with the email and pass given in the login form

--- login.js
+++ login.js
@@ -24,47 +24,27 @@ connection.connect((err) => {
 // Set up the HTTP server
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <body>
-        <form action="/" method="POST">
-          <label for="username">Username:</label>
-          <input type="text" name="username" id="username" /><br />
-          <label for="password">Password:</label>
-          <input type="password" name="password" id="password" /><br />
-          <button type="submit">Submit</button>
-        </form>
-      </body>
-    </html>
-  `);
-});
+    res.sendFile(__dirname + '/index.html');
+  });
 
 app.post('/', (req, res) => {
   const { username, password } = req.body;
 
-  // Insert the username and password into the database
+  // Check if the username and password match an existing record in the database
   connection.query(
-    'INSERT INTO loginuser (user_name, user_pass) VALUES (?, ?)',
+    'SELECT * FROM loginuser WHERE user_name = ? AND user_pass = ?',
     [username, password],
     (error, results, fields) => {
       if (error) {
-        console.error('Error inserting data: ' + error.stack);
+        console.error('Error querying database: ' + error.stack);
         res.send('An error occurred while processing your request.');
+      } else if (results.length > 0) {
+        //console.log('User authenticated');
+        //res.send('User authenticated.');
+        res.sendFile(__dirname + '/welcome.html');
       } else {
-        console.log('Inserted ' + results.affectedRows + ' rows');
-        //res.send('Data inserted successfully.');
-
-        console.log("hey");
-
-        connection.query("select * from loginuser where user_name = ? and user_pass = ?", 
-        
-        [username, password],function(error,results,fields){
-            if (results.length > 0){
-                res.redirect("./welcome");
-            }else{
-                console.log("Porraa");
-            }
-        })
+        console.log('User not found');
+        res.send('Username or password is incorrect.');
       }
     }
   );
